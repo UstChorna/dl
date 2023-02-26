a82aa66ccad26776d7623626948d8934345c59d8
UstChorna <CatcherRye@tutanota.com>
2023-02-26 14:41:41 +0200

    STAGE 3. Use Poseidon hash for did

--- baby_blockchain/did.go
+++ baby_blockchain/did.go
@@ -1,8 +1,9 @@
 package core
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/iden3/go-iden3-crypto/poseidon"
 )
 
 const (
@@ -13,25 +14,27 @@ const (
 
 // DID is a decentralized identifier.
 type DID struct {
-	ID      [32]byte
+	ID      string
 	Method  string
 	Chain   string
 	Network string
 }
 
 // NewDID creates a new DID.
-func NewDID(pubKey []byte) *DID {
-	// TODO: change to poseidon hash later.
-	id := sha256.Sum256(pubKey)
+func NewDID(pubKey []byte) (*DID, error) {
+	id, err := poseidon.HashBytes(pubKey)
+	if err != nil {
+		return nil, err
+	}
 	return &DID{
-		ID:      id,
+		ID:      id.Text(16),
 		Method:  DefaultMethod,
 		Chain:   DefaultChain,
 		Network: DefaultNetwork,
-	}
+	}, nil
 }
 
 // String returns the string representation of the DID.
 func (d DID) String() string {
-	return fmt.Sprintf("did:%s:%s:%s", d.Method, d.Network, d.Chain)
+	return fmt.Sprintf("did:%s:%s:%s:%s", d.Method, d.Network, d.Chain, d.ID)
 }
--- baby_blockchain/did_test.go
+++ baby_blockchain/did_test.go
@@ -2,15 +2,18 @@ package core_test
 
 import (
 	"crypto"
-	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"testing"
 
 	bjj "github.com/iden3/go-iden3-crypto/babyjub"
+	"github.com/iden3/go-iden3-crypto/poseidon"
 	core "github.com/ilya-korotya/distributed_lab/baby_blockchain"
 	"github.com/stretchr/testify/require"
 )
 
 var (
+	// generating by hands for testing
 	testingPK  = bjj.PrivateKey([32]byte{168, 140, 75, 205, 254, 48, 108, 255, 155, 247, 142, 64, 171, 137, 29, 147, 142, 40, 79, 195, 190, 95, 140, 232, 98, 131, 248, 41, 89, 74, 70, 151})
 	testingPUB = "f98147ef3a5e9e38d8d99b8a6635f020f079fa4cee783ca0ddb9c4c38414011f"
 )
@@ -27,7 +30,17 @@ func TestNewDID(t *testing.T) {
 			name:   "New DID",
 			pubKey: testingKeyPair.Public(),
 			want: &core.DID{
-				ID:      sha256.Sum256([]byte(testingPUB)),
+				ID: func() string {
+					bytesPub, err := hex.DecodeString(testingPUB)
+					if err != nil {
+						panic(err)
+					}
+					id, err := poseidon.HashBytes(bytesPub)
+					if err != nil {
+						panic(err)
+					}
+					return id.Text(16)
+				}(),
 				Method:  core.DefaultMethod,
 				Chain:   core.DefaultChain,
 				Network: core.DefaultNetwork,
@@ -38,8 +51,11 @@ func TestNewDID(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			pb := testingKeyPair.PublicToBytes()
-			got := core.NewDID(pb)
+			got, err := core.NewDID(pb)
+			require.NoError(t, err)
 			require.Equal(t, tt.want.String(), got.String())
+
+			fmt.Printf("did: %s for public key: %s", got.String(), testingPUB)
 		})
 	}
 }
